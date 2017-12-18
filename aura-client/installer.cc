a6fa5d8d8960ce04b7a3f5af432c936b9f7484f8
Randy Ramos <rramos1295@gmail.com>
2017-12-18 02:06:59 -0500

    [Client] Use create_directories instead of std::system calls
    I also reorganized some of the structure for Installer::InstallFiles

--- aura-client/installer.cc
+++ aura-client/installer.cc
@@ -32,7 +32,7 @@ std::string Installer::GetAuthHash() { return auth_->GetHash(); }
 #ifdef WIN32
 
 void Installer::InstallFiles() {
-    std::system(("mkdir " + install_dir_).c_str());
+    fs::create_directories(install_dir_);
     fs::copy_file(BIN, install_dir_ + BIN_NEW,
                   fs::copy_options::overwrite_existing);
 }
@@ -42,9 +42,7 @@ void Installer::InitRecurringJob() {
     std::string task_command = "schtasks.exe /create /F /tn " + TASK_NAME +
                                " /sc " + TASK_FREQ + " /mo " + TASK_FREQ_VALUE +
                                " /tr " + install_dir_ + BIN_NEW;
-    if (util::IsSuperuser()) {
-        task_command += " /rl highest";
-    }
+    task_command += util::IsSuperuser() ? " /rl highest" : "";
 
     std::system(task_command.c_str());
 }
@@ -53,34 +51,34 @@ void Installer::InitRecurringJob() {
 
 #ifdef __linux__
 
-#include <sys/stat.h>
-
 void Installer::InstallFiles() {
-    std::string mkdir_command, timer_path, bin_path;
+    if (not install_dir_.empty()) fs::create_directories(install_dir_);
 
-    bin_path = install_dir_ + BIN_NEW;
-    mkdir_command = "mkdir -p " + install_dir_ + " ";
-
+    fs::path timer_path = TIMER;
+    fs::path service_path = SERVICE;
+    std::string original_service = SERVICE;
     if (util::IsSuperuser()) {
-        mkdir_command += SYS_SERVICE_DEST;
-        timer_path = SYS_SERVICE_DEST + "/" + TIMER;
+        // Create directory for system systemd services
+        fs::create_directories(SYS_SERVICE_DEST);
 
         // If root, change service name back to normal after copying
-        fs::copy_file(SYS_SERVICE, SYS_SERVICE_DEST + "/" + SERVICE,
-                      fs::copy_options::overwrite_existing);
+        original_service = SYS_SERVICE;
+        service_path = SYS_SERVICE_DEST / service_path;
+        timer_path = SYS_SERVICE_DEST / timer_path;
     } else {
-        std::string home_dir = std::getenv("HOME");
-        std::string user_service_dir = home_dir + "/" + SERVICE_DEST;
+        // Create directory for storing user systemd services
+        fs::path user_service_dir = std::getenv("HOME");
+        user_service_dir /= SERVICE_DEST;
+        fs::create_directories(user_service_dir);
 
-        mkdir_command += user_service_dir;
-        timer_path = user_service_dir + "/" + TIMER;
-
-        fs::copy_file(SERVICE, user_service_dir + "/" + SERVICE,
-                      fs::copy_options::overwrite_existing);
+        service_path = user_service_dir / service_path;
+        timer_path = user_service_dir / timer_path;
     }
 
-    std::system(mkdir_command.c_str());
-    fs::copy_file(BIN, bin_path, fs::copy_options::overwrite_existing);
+    fs::copy_file(BIN, install_dir_ + BIN_NEW,
+                  fs::copy_options::overwrite_existing);
+    fs::copy_file(original_service, service_path,
+                  fs::copy_options::overwrite_existing);
     fs::copy_file(TIMER, timer_path, fs::copy_options::overwrite_existing);
 }
 
